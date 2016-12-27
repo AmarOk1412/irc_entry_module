757fd99c06bcfb4fa22f297f2a12362dd9deb1b2
AmarOk <blin.sebastien@enconn.fr>
2016-12-27 11:58:57 -0500

    get data from RORI

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,12 @@ use std::io::prelude::*;
 use std::path::Path;
 use std::net::{TcpListener, TcpStream};
 use std::str::from_utf8;
-use rori_utils::client::RoriClient;
+use rori_utils::client::{ConfigServer, RoriClient};
+use rori_utils::data::RoriData;
 use std::thread;
 use std::sync::{Arc, Mutex};
+use rustc_serialize::json::decode;
+use std::fs::File;
 
 struct Client {
     stream: TcpStream,
@@ -94,21 +97,67 @@ impl RoriIrcEntry {
     }
 }
 
-// Launch RoriIrcEntry
-fn main() {
-    // will contains messages from RORI
-    let vec = Arc::new(Mutex::new(Vec::new()));
-    let vec_cloned = vec.clone();
-    // TODO server from config file
-    let child = thread::spawn(move || {
-        let listener = TcpListener::bind("127.0.0.1:1413").unwrap();
+#[derive(Clone, RustcDecodable, RustcEncodable, Default, PartialEq, Debug)]
+struct RoriServer {
+    rori_ip: Option<String>,
+    rori_port: Option<String>,
+}
+
+pub struct Endpoint {
+    address: String,
+    rori_address: String,
+}
+
+impl Endpoint {
+    fn parse_config_server(data: String) -> String {
+        let params: ConfigServer = decode(&data[..]).unwrap();
+        format!("{}:{}",
+                &params.ip.unwrap_or(String::from("")),
+                &params.port.unwrap_or(String::from("")))
+    }
+
+    fn parse_config_rori(data: String) -> String {
+        let params: RoriServer = decode(&data[..]).unwrap();
+        format!("{}:{}",
+                &params.rori_ip.unwrap_or(String::from("")),
+                &params.rori_port.unwrap_or(String::from("")))
+    }
+
+    pub fn new<P: AsRef<Path>>(config: P) -> Endpoint {
+        // Configure from file
+        let mut file = File::open(config)
+            .ok()
+            .expect("Config file not found");
+        let mut data = String::new();
+        file.read_to_string(&mut data)
+            .ok()
+            .expect("failed to read!");
+        let address = Endpoint::parse_config_server(data.clone());
+        let rori_address = Endpoint::parse_config_rori(data);
+        if address == ":" || rori_address == ":" {
+            println!("Empty config for the connection to the server");
+        }
+        Endpoint {
+            address: address,
+            rori_address: rori_address,
+        }
+    }
+
+    pub fn start(&self, vec: Arc<Mutex<Vec<String>>>) {
+        let listener = TcpListener::bind(&*self.address).unwrap();
         for stream in listener.incoming() {
             match stream {
                 Ok(stream) => {
                     let mut client = Client::new(stream.try_clone().unwrap());
                     let content = client.read();
                     println!("RECEIVED:{}", &content);
-                    vec.lock().unwrap().push(content);
+                    let end = content.find(0u8 as char);
+                    let (content, _) = content.split_at(end.unwrap_or(content.len()));
+                    let data_to_process = RoriData::from_json(String::from(content));
+                    if data_to_process.datatype == "text" {
+                        println!("Push {}", &data_to_process.content);
+                        vec.lock().unwrap().push(data_to_process.content);
+                    }
                 }
                 Err(e) => {
                     println!("Connection failed because {}", e);
@@ -116,6 +165,29 @@ fn main() {
             };
         }
         drop(listener);
+    }
+
+    pub fn register(&self) {
+        // TODO security and if correctly registered
+        let rori_address = self.rori_address.clone();
+        let address = self.address.clone();
+        let mut client = RoriClient { address: rori_address };
+        let mut content = String::from(address);
+        content.push_str("|");
+        content.push_str("text");
+        client.send_to_rori("AmarOk", &*content, "irc_entry_module", "register")
+    }
+}
+
+// Launch RoriIrcEntry
+fn main() {
+    // will contains messages from RORI
+    let vec = Arc::new(Mutex::new(Vec::new()));
+    let vec_cloned = vec.clone();
+    let child = thread::spawn(move || {
+        let endpoint = Endpoint::new("config_endpoint.json");
+        endpoint.register();
+        endpoint.start(vec);
     });
 
     let rori = RoriIrcEntry::new("config_bot.json");
